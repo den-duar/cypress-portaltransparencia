0a108752b5ea225315dd96064a945458e50d9d58
den-duar <2021211510079@iesp.edu.br>
2023-12-04 00:28:54 -0300

    Finalização elementos

--- cypress/pages/OrgaosSecretarias/Elements.js
+++ cypress/pages/OrgaosSecretarias/Elements.js
@@ -1,4 +1,11 @@
 export const ELEMENTS = {
     botaoSecretarias: '[href="#/governo/estrutura"] > .mat-icon',
-    tituloSecUm: ':nth-child(1) > .card-header'
+    tituloSecUm: ':nth-child(1) > .card-header',
+    tituloSecDois: ':nth-child(2) > .card-header',
+    tituloSecTres: ':nth-child(3) > .card-header',
+    tituloSecQuartro: ':nth-child(4) > .card-header',
+    tituloSecCinco: ':nth-child(5) > .card-header',
+    tituloSecSeis: ':nth-child(6) > .card-header',
+    tituloSecSete: ':nth-child(7) > .card-header'
+
 }
--- cypress/pages/OrgaosSecretarias/Index.js
+++ cypress/pages/OrgaosSecretarias/Index.js
@@ -8,6 +8,13 @@ class OrgaosSecretarias {
 
     validarTelaSecretarias(){
         cy.get(el.tituloSecUm).should('contain', 'SETRAMP' )
+        cy.get(el.tituloSecDois).should('contain', 'CGM')
+        cy.get(el.tituloSecTres).should('contain', 'COMPDEC')
+        cy.get(el.tituloSecQuartro).should('contain', 'EMLUR')
+        cy.get(el.tituloSecCinco).should('contain', 'FUNJOPE')
+        cy.get(el.tituloSecSeis).should('contain', 'IPM')
+        cy.get(el.tituloSecSete).should('contain', 'PROGEM')
+        
     }
 }
 
